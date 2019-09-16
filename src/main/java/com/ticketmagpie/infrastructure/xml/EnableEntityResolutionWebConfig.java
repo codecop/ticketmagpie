33f8f9fc96df10596be779319bc1d4a7aa41b85e
Peter Kofler <peter.kofler@code-cop.org>
2019-09-16 20:51:00 +0200

    Remove @EnableWebMVC from API annotation as it destroys the UI/styles.

--- src/main/java/com/ticketmagpie/infrastructure/xml/EnableEntityResolutionWebConfig.java
+++ src/main/java/com/ticketmagpie/infrastructure/xml/EnableEntityResolutionWebConfig.java
@@ -5,10 +5,8 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableWebMvc
 @Configuration
 public class EnableEntityResolutionWebConfig extends WebMvcConfigurerAdapter {
 
