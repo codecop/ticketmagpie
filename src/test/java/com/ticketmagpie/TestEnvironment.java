4034dba03515f20527b8f0affbaec91df36dc2da
Peter Kofler <peter.kofler@code-cop.org>
2020-09-03 00:23:58 +0200

    Use environment values from System environment if it is there.

--- src/test/java/com/ticketmagpie/TestEnvironment.java
+++ src/test/java/com/ticketmagpie/TestEnvironment.java
@@ -1,22 +1,42 @@
 package com.ticketmagpie;
 
+import java.util.Properties;
+
 /**
  * Environment for integrated tests.
  */
 public class TestEnvironment {
 
-  private static final String HOST_PROPERTY_NAME = "magpie.host";
+  private static final String HOST_PROPERTY_NAME = "magpie.server";
   private static final String PORT_PROPERTY_NAME = "magpie.port";
 
-  public static String host(String host, int port) {
-    return "http://" + host + ':' + port;
+  public static boolean inEnvironment() {
+    Properties properties = System.getProperties();
+    return properties.containsKey(HOST_PROPERTY_NAME) && properties.containsKey(PORT_PROPERTY_NAME);
+  }
+
+  private static String host(String defaultHost) {
+    if (inEnvironment()) {
+      return System.getProperty(HOST_PROPERTY_NAME);
+    } else {
+      return defaultHost;
+    }
+  }
+
+  private static int port(int defaultPort) {
+    if (inEnvironment()) {
+      return Integer.getInteger(PORT_PROPERTY_NAME);
+    } else {
+      return defaultPort;
+    }
+  }
+
+  public static String server(String defaultHost, int defaultPort) {
+    return "http://" + host(defaultHost) + ':' + port(defaultPort);
   }
 
   public static String baseUrl(String host, int port) {
-    return host(host, port) + '/';
+    return server(host, port) + '/';
   }
-
-//  host = System.getProperty("");
-//  port = Integer.getInteger("magpie.port");
 
 }
