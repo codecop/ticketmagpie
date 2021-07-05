0041c5136519d3921950016d0c4eff73b3e43769
Peter Kofler <peter.kofler@code-cop.org>
2021-07-05 12:44:56 +0200

    Allow port to be set bz $PORT for Heroku.

--- src/main/java/com/ticketmagpie/Application.java
+++ src/main/java/com/ticketmagpie/Application.java
@@ -1,5 +1,8 @@
 package com.ticketmagpie;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication app = new SpringApplication(Application.class);
+    Map<String, Object> properties = new HashMap<>();
+    properties.put("server.port", "" + determinePort());
+    app.setDefaultProperties(properties);
+    app.run(args);
+  }
+
+  private static int determinePort() {
+    // get the Heroku assigned port
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+      return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    // else get the test assigned port
+    String port = System.getProperty("PORT", "8080"); // -DPORT=8080
+    return Integer.parseInt(port);
   }
 
 }
